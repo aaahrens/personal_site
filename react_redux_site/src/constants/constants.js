242c8ef6e403c3daa4cf3597b0e445b44454ce80
Adam Ahrens <drunkengranite@gmail.com>
2017-12-06 13:02:48 -0800

    build stuff

--- react_redux_site/src/constants/constants.js
+++ react_redux_site/src/constants/constants.js
@@ -23,22 +23,24 @@ export const BOILERPLATE_DESCRIPTION = "I have several boilerplates on my github
 
 
 export const SCRAPING_DESCRIPTION =
-	"This an example we scraper, using python's scrap and selenium framework. Its a fairly clean example" +
-	" because it has to parse a .Net application, which is like 90% js triggers, and not raw html. ";
+	"This an example web scraper, using python's scrapy and selenium framework. Its a solid bootstrapping project" +
+	" because it has to parse a .Net application (santarosa.edu), the majority of which are js triggers, and not raw html. It uses a mix of headless selenium and" +
+	" scrapy FormRequest. ";
 
 export const SYNONYMITY_DESCRIPTION =
 	"This project is an android app, an api and a python web scraper. The web scraper fetches the english dictionary and its synonyms from dictionary.com and loads them into a database." +
-	"The api generates problems sets and the android app makes a game guess what synonyms are which. i am currently converting it to a unity game with urbandictionary.com";
+	"The api generates problem sets and the android app makes a game to guess what synonyms are which. i am currently converting it to a unity game with urban dictionary.";
 
 
 export const AGGRE_DESCRIPTIONS = "A website designed to aggregate a fractured live-streaming market. Currently working with twitch and youtube. Aggre.tv includes a " +
-	"full stack golang api, python web scrapers and universal react client. Some of the functionality include custom playlists, and channel specific settings. I am currently working to port it to react native also. Email me for a look at the repos";
+	"full stack golang api, python web scrapers and universal react client. Some of the functionality includes custom playlists and channel specific settings. I am currently working to port it to react native. Email me for a look at the repos.";
 
 
-export const HALLA_DESCRIPTION =  "Back when I worked for these guys they were still a food recommendation platform. I created datascrapers based in both java and python and " +
-	"build an alpha-version android app";
+export const HALLA_DESCRIPTION =  "I worked for this company while they were only a food recommendation engine. My work included an android app (which is in alpha), backend webscraping" +
+	" and database admin. Almost all of what I wrote was in Java.";
 
-export const SKILLETS_DESCRIPTION= "Thw website of a cafe I regular at, and use a tech guinea pig. This uses google sheets as a CMS, SSR's and hydrates the" +
-	" redux store with data spreadsheet. Note I am working making it prettier :)";
+export const SKILLETS_DESCRIPTION= "The website of a cafe in my hometown. I use their site as a tech guinea pig. It currently uses google sheets as a CMS, SSR's and hydrates the" +
+	" redux store with data spreadsheets. This is a proof of concept on using spreadsheets as a very easily editable database.";
 
-export const ADAM_DESCRIPTION = "Includes the React-Redux version of this site, it has many features that are listed on the readme for the repo. The github also includes"
+export const ADAM_DESCRIPTION = "This website was written in react-redux. The github repository also contains the elm website, which is still a work in progress because I" +
+	"'m porting react-toolbox to elm. It's hosted on Scaleway, inside a docker container that is reverse proxied by nginx."
